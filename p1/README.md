b1f1ef619e9dfbf550f751f87fcc13f5bfda30bf
Bradley Ross <bradley@cs50.harvard.edu>
2023-02-06 19:49:06 +0000

    E15 Week2

--- p1/README.md
+++ p1/README.md
@@ -1,3 +1,11 @@
-# e15 Project 1
-## Bradley Ross
+# Project 1
++ By: Bradley Ross
++ URL: <http://e15p1.bradross.me>
+
+## Outside Resources
++ [regex for only letters](https://www.w3resource.com/php-exercises/php-regular-expression-exercise-7.php)
++ [strtolower - entire string](https://www.w3schools.com/php/func_string_strtolower.asp#:~:text=The%20strtolower()%20function%20converts,converts%20a%20string%20to%20uppercase)
+
+
+## Notes for Instructor
 
--- p1/index.php
+++ p1/index.php
@@ -1,2 +1,24 @@
-<h1>E15 Project 2</h1>
-<h2>Bradley Ross</h2>
+<?php
+
+# route "/"
+
+
+// select random word
+
+// hard coded inputs - use random from course examples
+
+// palindrome inputs (inputWord is fake user input)
+$palindromeExamples = array("racecar", "Racecar", "racecar!","!racecar!","Hello World");
+$random_key = array_rand($palindromeExamples);
+$inputWord = $palindromeExamples[$random_key];
+
+
+// sentence for vowel inputs (inputVowel is fake user input)
+$vowelExamples[] = "the quick brown fox jumps over the lazy dog";
+$vowelExamples[] = "Hll Wrld";
+$vowelExamples[] = "AeIoU";
+$random_key = array_rand($vowelExamples);
+$inputVowel = $vowelExamples[$random_key];
+
+
+require 'controllers/indexController.php';
